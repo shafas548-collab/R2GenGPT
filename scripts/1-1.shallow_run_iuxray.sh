a85d71f24ea2a9d2548fd5d34727c109e72aa4ac
shafas548-collab <shafas548@gmail.com>
2025-10-31 14:25:47 +0700

    try low shallow

--- scripts/1-1.shallow_run_iuxray.sh
+++ scripts/1-1.shallow_run_iuxray.sh
@@ -11,8 +11,8 @@ python -u train.py \
     --dataset ${dataset} \
     --annotation ${annotation} \
     --base_dir ${base_dir} \
-    --batch_size 8 \
-    --val_batch_size 12 \
+    --batch_size 2 \
+    --val_batch_size 2 \
     --freeze_vm True \
     --vis_use_lora False \
     --savedmodel_path ${savepath} \
@@ -21,7 +21,7 @@ python -u train.py \
     --max_new_tokens 100 \
     --repetition_penalty 2.0 \
     --length_penalty 2.0 \
-    --num_workers 8 \
+    --num_workers 4 \
     --devices 2 \
     --max_epochs 15 \
     --limit_val_batches 1.0 \
