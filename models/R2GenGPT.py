2513d65d140572a1d4d099ece7682d1168755895
shafas548-collab <shafas548@gmail.com>
2025-11-15 10:01:50 +0700

    fix bugs5

--- models/R2GenGPT.py
+++ models/R2GenGPT.py
@@ -222,18 +222,35 @@ class R2GenGPT(pl.LightningModule):
     # Prompt wrap (perbaikan utama: embed_tokens diambil ulang)
     # ============================================================
     def prompt_wrap(self, img_embeds, atts_img):
-        prompt=f'Human: <Img><ImageHere></Img> {self.prompt} \nAssistant:'
+        prompt = f'Human: <Img><ImageHere></Img> {self.prompt} \nAssistant:'
         batch_size = img_embeds.shape[0]
+
         p_before, p_after = prompt.split('<ImageHere>')
+
+        # ambil tokenizer
         p_before_tokens = self.llama_tokenizer(
-            p_before, return_tensors="pt", add_special_tokens=False).to(img_embeds.device)
+            p_before, return_tensors="pt", add_special_tokens=False
+        ).to(img_embeds.device)
+
         p_after_tokens = self.llama_tokenizer(
-            p_after, return_tensors="pt", add_special_tokens=False).to(img_embeds.device)
-        p_before_embeds = self.embed_tokens(p_before_tokens.input_ids).expand(batch_size, -1, -1)
-        p_after_embeds = self.embed_tokens(p_after_tokens.input_ids).expand(batch_size, -1, -1)
-        wrapped_img_embeds = torch.cat([p_before_embeds, img_embeds, p_after_embeds], dim=1)
+            p_after, return_tensors="pt", add_special_tokens=False
+        ).to(img_embeds.device)
+
+        # *** INI BAGIAN PENTING ***
+        embed_tokens = self._get_embed_tokens(img_embeds.device)
+
+        # embed teks
+        p_before_embeds = embed_tokens(p_before_tokens.input_ids).expand(batch_size, -1, -1)
+        p_after_embeds = embed_tokens(p_after_tokens.input_ids).expand(batch_size, -1, -1)
+
+        wrapped_img_embeds = torch.cat(
+            [p_before_embeds, img_embeds, p_after_embeds], dim=1
+        )
+
         wrapped_atts_img = atts_img[:, :1].expand(-1, wrapped_img_embeds.shape[1])
+
         return wrapped_img_embeds, wrapped_atts_img
+
 
     def _get_embed_tokens(self, device):
         """
