22f89b7d5ead70a83623f6ee1b3d1ea55ec88747
shafas548-collab <shafas548@gmail.com>
2025-11-25 11:39:48 +0700

    try vllm2

--- lightning_tools/callbacks.py
+++ lightning_tools/callbacks.py
@@ -8,33 +8,33 @@ from pytorch_lightning.callbacks import EarlyStopping
 
 
 def add_callbacks(args):
-log_dir = args.savedmodel_path
-os.makedirs(log_dir, exist_ok=True)
+    log_dir = args.savedmodel_path
+    os.makedirs(log_dir, exist_ok=True)
 
-# --------- Add Callbacks
-checkpoint_callback = ModelCheckpoint(
-dirpath=os.path.join(log_dir, "weights"),
-filename="{epoch}-{step}",
-save_top_k=-1,
-every_n_train_steps=args.every_n_train_steps,
-save_last=False,
-save_weights_only=False
-)
-
-lr_monitor_callback = LearningRateMonitor(logging_interval='step')
-tb_logger = pl_loggers.TensorBoardLogger(save_dir=os.path.join(log_dir, "logs"), name="tensorboard")
-csv_logger = CSVLogger(save_dir=os.path.join(log_dir, "logs"), name="csvlog")
-
-    early_stop_callback = EarlyStopping(
-        monitor="Bleu_4",
-        patience=8,
-        mode="max"
+    # --------- Add Callbacks
+    checkpoint_callback = ModelCheckpoint(
+    dirpath=os.path.join(log_dir, "weights"),
+    filename="{epoch}-{step}",
+    save_top_k=-1,
+    every_n_train_steps=args.every_n_train_steps,
+    save_last=False,
+    save_weights_only=False
     )
 
-to_returns = {
-        "callbacks": [checkpoint_callback, lr_monitor_callback],
-        "callbacks": [checkpoint_callback, lr_monitor_callback, early_stop_callback],
-"loggers": [csv_logger, tb_logger]
-}
+    lr_monitor_callback = LearningRateMonitor(logging_interval='step')
+    tb_logger = pl_loggers.TensorBoardLogger(save_dir=os.path.join(log_dir, "logs"), name="tensorboard")
+    csv_logger = CSVLogger(save_dir=os.path.join(log_dir, "logs"), name="csvlog")
 
-return to_returns
+        early_stop_callback = EarlyStopping(
+            monitor="Bleu_4",
+            patience=8,
+            mode="max"
+        )
+
+    to_returns = {
+            "callbacks": [checkpoint_callback, lr_monitor_callback],
+            "callbacks": [checkpoint_callback, lr_monitor_callback, early_stop_callback],
+    "loggers": [csv_logger, tb_logger]
+    }
+
+    return to_returns
